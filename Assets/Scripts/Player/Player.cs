1b4eaa92492803b05179a210563a1e102ee96b69
IRecs <68703540+IRecs@users.noreply.github.com>
2021-04-01 02:51:16 +0300

    Work

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,35 +5,5 @@ using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private GameObject _mainCamera;
-    [SerializeField] private GameObject _losePanel;
-    private int _firePower;
-    private int _score;
-
-    public event UnityAction<int> ChangedFirePower;
-    public int FirePower => _firePower;
-
-    private void Start()
-    {
-        ChangedFirePower?.Invoke(_firePower);
-    }
-
-    public void AddFirePower(int power)
-    {
-        if (power > 0)
-        {
-            _score += power;
-            _firePower = _score/4;
-        }
-        ChangedFirePower?.Invoke(_score);
-    }
-
-    public void Die()
-    {
-        /*PlayerPrefs.SetInt("OllScore", PlayerPrefs.GetInt("OllScore", 0) + _score);
-        PlayerPrefs.SetInt("LastScore", _score);
-        if(_score > PlayerPrefs.GetInt("TopScore", 0))
-            PlayerPrefs.SetInt("TopScore", _score);
-        _mainCamera.GetComponent<Menu>().RestartGame();*/
-    }
+    
 }
